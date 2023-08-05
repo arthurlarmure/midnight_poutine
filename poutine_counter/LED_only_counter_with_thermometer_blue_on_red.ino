db91a2d4c4b1277745c49cd8ee2818055741be61
arthurlarmure <65264564+arthurlarmure@users.noreply.github.com>
2023-08-05 16:12:05 -0400

    Add files via upload

---
+++ poutine_counter/LED_only_counter_with_thermometer_blue_on_red.ino
@@ -0,0 +1,91 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+#define PSTR // Make Arduino Due happy
+#endif
+
+#define PIN 9
+#define STRIP_PIN 4
+#define BUTTON_PIN 21 // The pin to which the button is connected
+
+// MATRIX DECLARATION:
+// ... (rest of the code remains unchanged)
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+
+int counter = 0; // The counter variable to keep track of the value
+bool buttonWasPressed = false; // Variable to track the previous button state
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(22, STRIP_PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(255);
+  matrix.setTextColor(matrix.Color(255, 0, 0)); // Set text color to red
+  
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // Set the button pin as input with internal pull-up resistor
+  Serial.begin(9600); // Initialize serial communication at 9600 baud rate
+}
+
+void loop() {
+  // Check if the button is pressed (active low)
+  bool buttonIsPressed = (digitalRead(BUTTON_PIN) == LOW);
+
+  // Check if the button is pressed and was not pressed before
+  if (buttonIsPressed && !buttonWasPressed) {
+    // Increment the counter when the button is initially pressed
+    counter++;
+
+    // Update the display with the new counter value
+    updateDisplay();
+    updateThermometer();
+  }
+
+  // Update the previous button state
+  buttonWasPressed = buttonIsPressed;
+}
+
+void updateDisplay() {
+  matrix.fillScreen(0);
+  
+  // Convert the counter value to a string for display
+  char counterStr[6]; // Assuming the counter won't exceed 99999
+  snprintf(counterStr, sizeof(counterStr), "%d", counter);
+  
+  // Calculate the center x-coordinate for the text
+  int textWidth = strlen(counterStr) * 6; // Assuming each character is 6 pixels wide
+  int centerX = (matrix.width() - textWidth) / 2;
+  
+  matrix.setCursor(centerX, 0); // Set cursor to the calculated x-coordinate
+  matrix.print(counterStr);
+  matrix.show();
+
+  // Send the counter value to the Serial Monitor
+  Serial.print("Counter: ");
+  Serial.println(counter);
+}
+
+
+void updateThermometer() {
+  int goal = 50;
+  int thermometerPixels = map(counter, 0, goal, 0, strip.numPixels());
+
+  for (int i = 0; i < strip.numPixels(); i++) {
+    if (i < thermometerPixels) {
+      strip.setPixelColor(i, strip.Color(0, 0, 255)); // Blue when counter progresses
+    } else {
+      strip.setPixelColor(i, strip.Color(255, 0, 0)); // Red when counter goal is reached
+    }
+  }
+
+  strip.show();
+}
+
+
+ 
